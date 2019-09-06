e7cc5dc138cb39c7a1f3a75588c5c71d11522e32
Sumitro Bhaumik <thexavier666@users.noreply.github.com>
2019-09-06 22:50:06 +0530

    Update chk_season_end.py

--- chk_season_end.py
+++ chk_season_end.py
@@ -31,7 +31,7 @@ def main():
 	bottle.route("/season_status", method='GET')(get_season_status)
 
 	if os.environ.get('APP_LOCATION') == 'heroku':
-		bottle.run(host = "0.0.0.0", port = int(os.environ.get("PORT", 8888)))
+		bottle.run(host = "0.0.0.0", port = int(os.environ.get("PORT", 5000)))
 	else:
 		bottle.run(host = "localhost", port = 8888, debug = True)
 
