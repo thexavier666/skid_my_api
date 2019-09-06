ef9fc929022a6befb06c8ec13ad5427629ebafd8
Sumitro Bhaumik <thexavier666@users.noreply.github.com>
2019-09-06 22:41:29 +0530

    1st commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python3 chk_season_end.py
---
+++ chk_season_end.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import time
+import json
+import requests
+import os
+import bottle
+
+from datetime import datetime
+
+def get_season_status():
+	
+	url_str = 'http://api.skidstorm.cmcm.com/v2/rank/list/1-1/ALL'
+	rank_threshold = 11000
+	
+	p = requests.get(url_str)
+	q = json.loads(p.text)
+	
+	rank_val = q["ranks"][0]["rank"]
+	
+	resp_dict = {'season_reset':'','current_time':str(datetime.now())}
+	
+	if rank_val > rank_threshold:
+		resp_dict['season_reset'] = False
+	else:
+		resp_dict['season_reset'] = True 
+	
+	return resp_dict
+
+def main():
+	bottle.route("/season_status", method='GET')(get_season_status)
+
+	if os.environ.get('APP_LOCATION') == 'heroku':
+		bottle.run(host = "0.0.0.0", port = int(os.environ.get("PORT", 8888)))
+	else:
+		bottle.run(host = "localhost", port = 8888, debug = True)
+
+if __name__ == '__main__':
+	main()
