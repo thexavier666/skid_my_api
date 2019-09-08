b8f10ecc0e051267de8d103c10179f96db88e77d
thexavier666 <sumitros@gmail.com>
2019-09-08 20:51:12 +0530

    added a small webpage

--- chk_season_end.py
+++ chk_season_end.py
@@ -9,32 +9,59 @@ import bottle
 from datetime import datetime
 
 def get_season_status():
-	
-	url_str = 'http://api.skidstorm.cmcm.com/v2/rank/list/1-1/ALL'
-	rank_threshold = 11000
-	
-	p = requests.get(url_str)
-	q = json.loads(p.text)
-	
-	rank_val = q["ranks"][0]["rank"]
-	
-	resp_dict = {'season_reset':'','current_time':str(datetime.now())}
-	
-	if rank_val > rank_threshold:
-		resp_dict['season_reset'] = False
-	else:
-		resp_dict['season_reset'] = True 
-	
-	return resp_dict
+    
+    url_str = 'http://api.skidstorm.cmcm.com/v2/rank/list/1-1/ALL'
+    rank_threshold = 11000
+    
+    p = requests.get(url_str)
+    q = json.loads(p.text)
+    
+    resp_dict = {}
+
+    resp_dict['fetch_time'] = str(datetime.now())
+    resp_dict['rank_value'] = q["ranks"][0]["rank"]
+    resp_dict['username']   = q["ranks"][0]["username"]
+    
+    if resp_dict['rank_value'] > rank_threshold:
+        resp_dict['season_reset'] = False
+    else:
+        resp_dict['season_reset'] = True 
+    
+    return resp_dict
+
+def web_season_status():
+    resp_dict = get_season_status()
+
+    web_page_str = '''
+    <html>
+        <body>
+            <h3>Season reset status : {}</h3><br><br>
+            Trophies : {}<br>
+            Name : {}<br>
+            Rank : {}<br>
+            Data fetch time : {}<br>
+            Timezone : {}
+        </body>
+    </html>
+    '''.format(resp_dict['season_reset'], resp_dict['rank_value'], resp_dict['username'], 1, resp_dict['fetch_time'], time.tzname[1])
+
+    return web_page_str
 
 def show_default_page():
-    return "YES, THIS PAGE WORKS TOO, UNLIKE CHEETAH :P\n\n--xavier666"        
-
+    web_page_str = '''
+    <html>
+        <body>
+            YES, THIS PAGE WORKS TOO, UNLIKE CHEETAH<br>
+            --xavier666
+        </body>
+    </html>
+    '''
+    return web_page_str
 def main():
-	bottle.route("/", method='GET')(show_default_page)
-	bottle.route("/season_status", method='GET')(get_season_status)
-	
-	bottle.run(host = "0.0.0.0", port = int(os.environ.get("PORT", 10000)), debug = False)
+    bottle.route("/", method='GET')(show_default_page)
+    bottle.route("/season_status", method='GET')(web_season_status)
+    
+    bottle.run(host = "0.0.0.0", port = int(os.environ.get("PORT", 10000)), debug = False)
 
 if __name__ == '__main__':
-	main()
+    main()
